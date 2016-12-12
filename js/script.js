675a12ff6504fb440c8eaeb13428b7a24c3b0af2
Lina Breslav <linabreslav@Lina-Breslav1s-MacBook-Pro-2.local>
2016-12-12 08:07:38 -0500

    pre-push save

--- js/script.js
+++ js/script.js
@@ -8,4 +8,15 @@
       arrows: false,
       animation: 'fade'
     });
-  });
+
+
+  var objGroup = document.getElementsByClassName('ratingSelector');
+  for (var i = 0; i < objGroup.length; i++) {
+    var objRadio = [].slice.call(objGroup[i].getElementsByClassName('full')); // Create an array of items
+    for (var j = objRadio.length; j--;) { // Loop through the array backwards
+      var checkbox = document.getElementById(objRadio[j].getAttribute('for'));
+      objGroup[i].appendChild(checkbox);
+      objGroup[i].appendChild(objRadio[j]);
+    }
+  }
+});
